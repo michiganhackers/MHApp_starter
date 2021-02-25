bf2de58d23c5e2503221938e9ee53f2c51282251
Thomas Smith <thsm@umich.edu>
2021-02-25 18:56:56 -0500

    Add comments to Announcements VC and add gitignore

--- MHApp_starter/AnnouncementsViewController.swift
+++ MHApp_starter/AnnouncementsViewController.swift
@@ -5,6 +5,30 @@
 //  Created by Yatharth Chhabra on 2/24/21.
 //
 
+/*
+    TODO: This is the announcements screen of the app. This screen should show the same
+          information that you get in the weekly email, and eventually, you should be able
+          to look at previous weeks' announcements too.
+    
+    Tips:
+    - The announcements for each week can be loaded from
+      https://michiganhackers.github.io/mh-ios-app-backend/announcements/<day>,
+      where <day> is the Sunday when the announcements were emailed out.
+      For example, the announcements for February 21-27 can be loaded from
+      https://michiganhackers.github.io/mh-ios-app-backend/announcements/2021-02-21
+ 
+    - Consider using a WKWebView to load the announcements from the above URL.
+ 
+    - If you use a WKWebView, you might want to change its configuration so
+      links that you click take you outside of the Michigan Hackers app into Safari.
+
+    - Add buttons to let you navigate from week to week. Make sure you handle the
+      case when there aren't any announcements on the website for some week!
+ 
+    - Feel free to change the colors and style of the announcements page once you
+      have it working!
+ */
+
 import UIKit
 
 class AnnouncementsViewController: UIViewController {
